8d29491c552322b4eb551c95191c2681d3b0eaba
Awwal Adeleke <aadeleke@afexnigeria.com>
2022-12-02 10:33:40 +0100

    Updated

--- website/settings.py
+++ website/settings.py
@@ -159,8 +159,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, "static")
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIR = [STATIC_ROOT, ]
+
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = "media/"
 # DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
--- website/urls.py
+++ website/urls.py
@@ -56,3 +56,5 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+#     static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
