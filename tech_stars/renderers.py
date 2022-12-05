d707f40f7cd55ba502af3f3b685305a8f9cfa56d
AbrahamAdekunle <107469961+AbrahamAdekunle@users.noreply.github.com>
2022-12-05 10:40:41 +0100

    fixed renderer

--- tech_stars/renderers.py
+++ tech_stars/renderers.py
@@ -5,14 +5,15 @@ from .enc_dec.encryption_decryption import aes_encrypt
 class CustomRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         status_code = renderer_context["response"].status_code
-        response = {
-            "success": True,
-            "status_code": status_code,
-            "data": aes_encrypt(data),
-            "message": "Successfully"
-        }
+        if str(status_code).startswith("2"):
+            response = {
+                "success": True,
+                "status_code": status_code,
+                "data": aes_encrypt(data),
+                "message": "Successfully"
+            }
 
-        if not str(status_code).startswith("2"):
+        else:
             response = {"success": False, "status_code": status_code}
             try:
                 response["data"] = data
